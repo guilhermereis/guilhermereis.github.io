18c049efc422d77685592e10518d1be335c7bac9
Guilherme Marques <agoravaiguilherme@gmail.com>
2020-02-02 16:06:33 -0300

    Updates

---
+++ precache-manifest.c8f16a67507ebfc58f17673a2aec5701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70cde48612456fe54c7b4321faab624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "294738885a509f2544b5",
+    "url": "/static/js/2.3d0b4f91.chunk.js"
+  },
+  {
+    "revision": "5ccb43a6acad5975028aafb8c47cfa56",
+    "url": "/static/js/2.3d0b4f91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0af9ff08942c09db81d1",
+    "url": "/static/js/main.9a20224c.chunk.js"
+  },
+  {
+    "revision": "53790ce221472c50085c",
+    "url": "/static/js/runtime-main.12ee56c6.js"
+  }
+]);
